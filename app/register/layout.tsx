eac03fcd61b7e84ef478116ae90154deaf042454
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-31 00:24:04 -0400

    removed nav from layout

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -1,12 +1,13 @@
-import SideNav from '@/ui/sidenav';
- 
+// app/layout.tsx or app/register/layout.tsx
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
+    <>
+      <div className="min-h-screen">
+        <main className="flex justify-center items-center">
+          {children}
+        </main>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
-    </div>
+    </>
   );
 }
