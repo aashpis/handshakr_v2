a0987837f1b3660483f05658e5d0220cc8fdcaa7
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-31 00:02:20 -0400

    disabled rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific rules
+      "@typescript-eslint/no-explicit-any": "off", // Turn off the rule for any
+      "@typescript-eslint/no-unused-vars": "off", // Turn off unused-vars
+      "@typescript-eslint/no-empty-object-type": "off", // Disable empty object type error
+    },
+  },
 ];
 
 export default eslintConfig;
