b8c1fb768f5b3974f345f5f6d576c30a7e075dd7
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-30 23:52:33 -0400

    style update

--- app/ui/handshake-analytics-card.jsx
+++ app/ui/handshake-analytics-card.jsx
@@ -1,8 +1,22 @@
-export default function HandshakeAnalyticsCard({count, type}){
-    return(
-        <div className="bg-white my-4 text-grey-700 text-sm border rounded">
-            <p className="text-amber-700 text-lg font-bold ">{count}</p>
-            <p>{type}</p>
+export default function HandshakeAnalyticsCard({ count, status }) {
+  const sanitizedStatus = status.toLowerCase();
+  // Define background styles based on the status
+  const STATUS_BG_STYLE = {
+    completed: "bg-green-100",
+    failed: "bg-red-100",
+    pending: "bg-amber-100",
+  }[sanitizedStatus] || "bg-gray-100"; // Default to gray if no match
+ 
+  return (
+    <div className="flex flex-row rounded-lg shadow-md">
+      <div className={`${STATUS_BG_STYLE} flex-1 flex items-center justify-center`}>
+        <div className="text-lg font-bold p-2">
+          <p className="capitalize">{status}</p>
         </div>
-    )
+      </div>
+      <div className="bg-white rounded-lg p-4 text-2xl font-semibold flex items-center justify-center w-[120px]">
+        <p>{count}</p>
+      </div>
+    </div>
+  );
 }
