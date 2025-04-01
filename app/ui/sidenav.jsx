895948e8fc5ce10dfcf80cc2b2ec11c22b2abca9
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-01 18:57:08 -0400

    added icon

--- app/ui/sidenav.jsx
+++ app/ui/sidenav.jsx
@@ -8,15 +8,16 @@ export default function SideNav() {
   return (
     <div className="flex h-full flex-col bg-white px-3 py-4 md:px-2">
       <Link
-        className="mb-2 flex h-20 items-center p-4 md:h-40 bg-primary"
+        className="mb-2 flex h-20 items-center p-4 md:h-40"
         href="/"
       >
         <Image
-            src="/handshake.jpg"
+            src="/hs_icon.png"
+            // src="/handshakr-namebanner.png"
             width={1250}
             height={625}
             alt="handshake"
-            className="w-full rounded-md"
+            className='object-scale-down'
           />
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
