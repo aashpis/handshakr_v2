03a4194c3c33b28f0035ff29ec04b0f5faa40a52
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-31 21:39:16 -0400

    init commit

---
+++ app/pending/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+import HandshakeCard from "@/ui/handshake-card"
+
+const handshake = {
+    id: "12345",
+    initiator: "Shankar",
+    agreerer: "Ari",
+    compensation: "$20",
+    terms: "I will sell you a used computer mouse for $20",
+    status: "pending",
+    notarized: false
+}
+
+export default function Page() {
+    return (
+    <div className="flex flex-col items-center justify-top min-h-screen">
+        <h1>Pending Handshakes</h1>
+        <HandshakeCard {...handshake} />
+    </div>
+
+    );
+}
