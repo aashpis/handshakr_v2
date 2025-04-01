61dbf08df02f6db01d0f54b0c8dd2e7ade53cdd2
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-01 18:56:50 -0400

    styling and syntax

--- app/ui/handshake-creation-form.jsx
+++ app/ui/handshake-creation-form.jsx
@@ -27,15 +27,17 @@ export default function HandshakeCreationForm() {
         </div> */}
 
         <div className="mb-4">
-          <label className="block text-sm font-medium  mb-2" htmlFor="acceptorUsername">
+          <label 
+            className="block text-sm font-medium  mb-2" 
+            htmlFor="agreererUsername">
             Agreerer Username
           </label>
           <input
-            id="acceptorUsername"
-            name="acceptorUsername"
+            id="agreererUsername"
+            name="agreererUsername"
             placeholder="Username"
             required
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+            className="w-full px-4 py-2 border border-neutral-light rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-light focus:border-transparent"
           />
         </div>
 
