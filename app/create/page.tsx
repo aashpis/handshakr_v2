3ce50d24b73b720b2eb8ff62186f75d9f8197b53
aashpis <37486859+aashpis@users.noreply.github.com>
2025-03-31 21:31:37 -0400

    init commit

---
+++ app/create/page.tsx
@@ -0,0 +1,12 @@
+import HandshakeCreationForm from '@/ui/handshake-creation-form'
+
+
+export default function Page() {
+  return (
+      <div className="flex items-center justify-center">
+        <HandshakeCreationForm />
+      </div>
+
+  );
+}
+ 
