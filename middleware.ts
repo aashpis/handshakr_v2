7d4d6f7a7574f5580bbc80d837775d8c78ebe3fb
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-01 23:06:26 -0400

    commented out code to bypass for testing

--- middleware.ts
+++ middleware.ts
@@ -15,40 +15,40 @@ export async function middleware(req: NextRequest) {
   console.log(`Middleware running for: ${req.nextUrl.pathname}`);
   
   
-  // check if public asset to bypass
-  if (publicAssets.some(asset => url.pathname.startsWith(asset))) {
-    return NextResponse.next();
-  }
+  // // check if public asset to bypass
+  // if (publicAssets.some(asset => url.pathname.startsWith(asset))) {
+  //   return NextResponse.next();
+  // }
   
-  // direct to public routes, no auth needed
-  if (publicRoutes.some(route => url.pathname === route || url.pathname.startsWith(`${route}/`))) {
-    return NextResponse.next();
-  }
+  // // direct to public routes, no auth needed
+  // if (publicRoutes.some(route => url.pathname === route || url.pathname.startsWith(`${route}/`))) {
+  //   return NextResponse.next();
+  // }
   
-  // check if protected route
-  if (protectedRoutes.some((route) => url.pathname.startsWith(route))) {
-    // Read JWT directly from request cookies
-    const jwt = req.cookies.get("jwt_token")?.value;
+  // // check if protected route
+  // if (protectedRoutes.some((route) => url.pathname.startsWith(route))) {
+  //   // Read JWT directly from request cookies
+  //   const jwt = req.cookies.get("jwt_token")?.value;
     
-    if (!jwt) {
-      // Redirect to login if no valid JWT
-      return NextResponse.redirect(new URL("/", req.url));
-    }
+  //   if (!jwt) {
+  //     // Redirect to login if no valid JWT
+  //     return NextResponse.redirect(new URL("/", req.url));
+  //   }
     
-    // add jwt to header
-    // TODO: do I need to add csrf?
-    const requestHeaders = new Headers(req.headers);
-    requestHeaders.set("Authorization", `Bearer ${jwt}`);
+  //   // add jwt to header
+  //   // TODO: do I need to add csrf?
+  //   const requestHeaders = new Headers(req.headers);
+  //   requestHeaders.set("Authorization", `Bearer ${jwt}`);
     
-    return NextResponse.next({
-      request: {
-        headers: requestHeaders,
-      },
-    });
-  }
+  //   return NextResponse.next({
+  //     request: {
+  //       headers: requestHeaders,
+  //     },
+  //   });
+  // }
   
-  // For all other routes, proceed
-  return NextResponse.next();
+  // // For all other routes, proceed
+  // return NextResponse.next();
 }
 export const config = {
   matcher: [
