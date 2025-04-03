3305582d3856852da6802075d766fcb2f7d6c167
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-03 14:26:03 +0000

    fix redirection - useEffect for redirection after successful user registration

--- app/_ui/user-registration-form.jsx
+++ app/_ui/user-registration-form.jsx
@@ -1,75 +1,89 @@
-'use client'
+"use client";
 
-import { registerNewUser } from '../_lib/auth'
-import { useActionState } from 'react'
+import { registerNewUser } from "@/_lib/auth";
+import { useActionState } from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function UserRegistrationForm() {
-  const [state, action, pending] = useActionState(registerNewUser, undefined)
+  const router = useRouter();
+  const [state, action, pending] = useActionState(registerNewUser, undefined);
+
+  // Redirect when registration is successful
+  useEffect(() => {
+    if (state?.success) {
+      router.push("/dashboard");
+    }
+  }, [state, router]);
 
   return (
     <div className="flex items-center justify-center min-h-screen ">
-      <div className='rounded-lg shadow-md'>
-      <form
-        action={action}
-        className="bg-white px-8 pt-6 pb-8 mb-4"
-        >
-        <div className="mb-4">
-          <label
-            className="block text-sm font-bold mb-2"
-            htmlFor="username">
-            Username
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
-            id="username" 
-            name="username"
-            placeholder="Username" 
+      <div className="rounded-lg shadow-md">
+        <form action={action} className="bg-white px-8 pt-6 pb-8 mb-4">
+          <div className="mb-4">
+            <label className="block text-sm font-bold mb-2" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="username"
+              name="username"
+              placeholder="Username"
             />
-        </div>
-        {state?.errors?.username && <p className='text-red-500 mb-4'>{state.errors.name}</p>}
-
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-            htmlFor="email">
-            Email
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="email" 
-            name="email" 
-            placeholder="Email" />
-        </div>
-        {state?.errors?.email && <p className='text-red-500 mb-4'>{state.errors.email}</p>}
-
-        <div className="mb-4">
-          <label
-            className="block text-gray-700 text-sm font-bold mb-2"
-          >
-            Password
-          </label>
-          <input
-            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            id="password" name="password" type="password" />
-        </div>
-        {state?.errors?.password && (
-          <div>
-            <p className='text-red-500'>Password must:</p>
-            <ul className = "text-sm mb-4">
-              {state.errors.password.map((error) => (
-                <li key={error}>- {error}</li>
-              ))}
-            </ul>
           </div>
-        )}
+          {state?.errors?.username && (
+            <p className="text-red-500 mb-4">{state.errors.username}</p>
+          )}
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+          {state?.errors?.email && (
+            <p className="text-red-500 mb-4">{state.errors.email}</p>
+          )}
+
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+            />
+          </div>
+          {state?.errors?.password && (
+            <div>
+              <p className="text-red-500">Password must:</p>
+              <ul className="text-sm mb-4">
+                {state.errors.password.map((error) => (
+                  <li key={error}>- {error}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+
           <button
             className="w-full bg-blue-500 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
             disabled={pending}
-            type="submit">
+            type="submit"
+          >
             Sign Up
           </button>
-      </form>
+        </form>
+      </div>
     </div>
-  </div>
-  )
+  );
 }
