8f678144fe09ff58fd815cbc28eeac80e0708318
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-06 12:20:31 -0400

    added handshake name field

--- app/_ui/handshake-creation-form.jsx
+++ app/_ui/handshake-creation-form.jsx
@@ -29,6 +29,21 @@ export default function HandshakeCreationForm() {
         <div className="mb-4">
           <label 
             className="block text-sm font-medium  mb-2" 
+            htmlFor="handshakeName">
+            Handshake Name
+          </label>
+          <input
+            id="handshakeName"
+            name="handshakeName"
+            placeholder="handshakeName"
+            required
+            className="w-full px-4 py-2 border border-neutral-light rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-light focus:border-transparent"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label 
+            className="block text-sm font-medium  mb-2" 
             htmlFor="agreererUsername">
             Agreerer Username
           </label>
