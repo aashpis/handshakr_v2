3374f286af4d0ae1942bdc5238ed817fdf239a6f
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-08 17:13:53 +0000

    styling update

--- app/_ui/page-header.jsx
+++ app/_ui/page-header.jsx
@@ -1,7 +1,7 @@
 export default function PageHeader ({title, subTitle}) {
     return (
         <div className="mb-4">
-            <h1 className="text-primary">{title}</h1>
+            <h1 className="text-primary font-bold text-3xl">{title}</h1>
             {subTitle && <h2 className="italic">{subTitle}</h2>}
         </div>
     );
