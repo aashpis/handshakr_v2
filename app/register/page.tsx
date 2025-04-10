71ffd6f30713453c73ea6efbd55b30ad0c30594d
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-09 21:21:08 -0400

    restructuring page

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
 import UserRegistrationForm from '@/_ui/user-registration-form'
 
 export default function Page() {
   return (
-   <UserRegistrationForm />
+    <div className="flex flex-col items-center justify-start min-h-screen">
+      <div className="w-full max-w-xs bg-white rounded-lg shadow-md overflow-hidden mt-10">
+        <Image
+          src="/handshakr-banner.png"
+          width={1153}
+          height={370}
+          alt="handshakr banner"  
+          className="w-full h-auto"
+        />
+        <UserRegistrationForm />
+      </div>
+    </div>
   );
 }
-
-
