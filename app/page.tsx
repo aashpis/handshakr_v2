14b6732da3ae7b4e0ff4cdc58df35aa65d488e34
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-09 21:26:23 -0400

    added createnewaccountbutton component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
+import Image from 'next/image';
 import LoginForm from "@/_ui/login-form"
+import CreateNewAccountButton from "@/_ui/create-new-account-button"
 
 export default function Home() {
   return (
-   <LoginForm />
+    <div className="flex flex-col items-center justify-start min-h-screen">
+      <div className="w-full max-w-xs bg-white rounded-lg shadow-md overflow-hidden mt-10">
+        <Image
+          src="/handshakr-banner.png"
+          width={1153}
+          height={370}
+          alt="handshakr banner"  
+          className="w-full h-auto"
+        />
+        
+        <div className="p-4">
+          <LoginForm />
+          <CreateNewAccountButton />
+        </div>
+      </div>
+    </div>
   );
 }
