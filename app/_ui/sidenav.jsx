92e79cab462a1e43c7cf6e8711795d78b1de1018
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-09 23:30:01 -0400

    logout button import fix

--- app/_ui/sidenav.jsx
+++ app/_ui/sidenav.jsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import NavLinks from './nav-links';
-import LogoutButton from '@/ui/logout-button';
+import LogoutButton from './logout-button';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { Menu, X } from 'lucide-react'; // Optional: install lucide-react for icons
