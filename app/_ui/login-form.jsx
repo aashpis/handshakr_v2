ddd76e7178e1ea5b97dec8b77d11b02a97a3152a
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-09 21:27:34 -0400

    separated create new account button into component

--- app/_ui/login-form.jsx
+++ app/_ui/login-form.jsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from 'next/image';
 import Link from 'next/link';
 import { useActionState } from 'react';
 import { useEffect } from 'react';
@@ -19,75 +18,53 @@ export default function LoginForm() {
   }, [state, router]);
 
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="w-full max-w-xs rounded-lg">
-        <div className="relative">
-          <h1 className="absolute text-5xl font-teko bottom-0 left-1/2 -translate-x-1/2">
-            Handshākr
-          </h1>
-          <Image
-            src="/handshake.jpg"
-            width={1250}
-            height={625}
-            alt="handshake"
-            className="w-full"
+
+    <div className="w-full max-w-xs rounded-lg">
+      <form action={action} className="bg-white px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label
+            className="block text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="w-full py-2 px-3 leading-tight shadow appearance-none border rounded border-neutral-dark focus:ring-primary focus:outline-none focus:shadow-outline"
+            id="username"
+            name="username"
+            type="text"
+            placeholder="Username"
+            required
           />
         </div>
-        <form action={action} className="bg-white px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label
-              className="block text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Username
-            </label>
-            <input
-              className="w-full py-2 px-3 leading-tight shadow appearance-none border rounded border-neutral-dark focus:ring-primary focus:outline-none focus:shadow-outline"
-              id="username"
-              name="username"
-              type="text"
-              placeholder="Username"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label className="block text-sm font-bold mb-2" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              id="password"
-              name="password"
-              type="password"
-              placeholder="password"
-              required
-            />
-          </div>
-          <div className="flex items-center justify-between">
-            <Link
-              className="inline-block align-baseline font-bold text-sm text-primary hover:text-primary-dark"
-              href="/forgot-password"
-            >
-              Forgot Password?
-            </Link>
-            <button
-              className="bg-primary hover:bg-primary-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="submit"
-              disabled={pending}
-            >
-              {pending ? 'Signing in...' : 'Sign In'}
-            </button>
-          </div>
-          {state?.message && (
-            <p className="text-warning text-center font-semibold my-3">
-              Login Failed
-            </p>
-          )}
-        </form>
-        <div className="flex items-center justify-center mt-5 bg-primary text-white py-4 px-4 font-bold text-sm rounded cursor-pointer focus:outline-none focus:shadow-outline hover:bg-primary-dark">
-          <Link href="/register">Create New Account</Link>
+        <div className="mb-6">
+          <label className="block text-sm font-bold mb-2" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="password"
+            required
+          />
         </div>
-      </div>
+        <div className="flex justify-end">
+          <button
+            className="w-full bg-primary text-white py-4 px-4 font-bold text-sm rounded cursor-pointer focus:outline-none focus:shadow-outline hover:bg-primary-dark"
+            type="submit"
+            disabled={pending}
+          >
+            {pending ? 'Signing in...' : 'Sign In'}
+          </button>
+        </div>
+        {state?.message && (
+          <p className="text-warning text-center font-semibold my-3">
+            Login Failed
+          </p>
+        )}
+      </form>
     </div>
   );
 }
