f55d1bb8e8a5423a05b00507ad854e442a6218a4
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-09 21:26:44 -0400

    init commit

---
+++ app/_ui/create-new-account-button.jsx
@@ -0,0 +1,19 @@
+// CreateNewAccountButton.jsx
+'use client'
+import { useRouter } from 'next/navigation';
+
+export default function CreateNewAccountButton() {
+  const router = useRouter();
+   
+  return (
+    <div className="w-full max-w-xs">
+      <div className="px-8">  {/* Add padding to match the form padding */}
+        <button
+          onClick={() => router.push("/register")}
+          className="w-full bg-white border-solid border-primary border-2 text-primary py-4 px-4 font-bold text-sm rounded cursor-pointer focus:outline-none focus:shadow-outline hover:bg-primary-dark hover:border-primary-dark hover:text-white">
+          Create New Account
+        </button>
+      </div>
+    </div>
+  );
+}
