f644ff241c8ee89056e60ca8eb0d7acf1fd241fd
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-09 22:36:50 -0400

    included cookie deletion in handleLogout

--- app/_ui/logout-button.jsx
+++ app/_ui/logout-button.jsx
@@ -3,6 +3,7 @@
 import { logoutUser } from '@/_lib/auth/logoutUser';
 import { useRouter } from 'next/navigation';
 import { useTransition } from 'react';
+import { LogOut } from 'lucide-react';
 
 
 export default function LogoutButton() {
@@ -11,6 +12,12 @@ export default function LogoutButton() {
 
   const handleLogout = async () => {
     await logoutUser();
+    const cookieStore = cookies();
+    // delete cookies
+    cookieStore.getAll().forEach((cookie) => {
+      cookieStore.delete(cookie.name);
+    });
+
     startTransition(() => {
       router.push('/');
     });
@@ -18,13 +25,16 @@ export default function LogoutButton() {
 
   return (
     <button
-      onClick={handleLogout}
-      disabled={isPending}
-      className="w-full h-auto items-center bg-primary text-white font-bold text-sm hover:bg-warning hover:text-white md:justify-start md:p-2 md:px-3 disabled:opacity-50"
-    >
+    onClick={handleLogout}
+    disabled={isPending}
+    className="w-full h-auto items-center bg-primary text-white font-bold text-sm hover:bg-warning hover:text-white md:justify-start md:p-2 md:px-3 disabled:opacity-50"
+  >
+    <div className="flex items-center justify-center gap-2">
+      <LogOut className="w-4 h-4" />
       <div className="w-full text-center">
         {isPending ? 'Logging out...' : 'Log Out'}
       </div>
-    </button>
-  );
+    </div>
+  </button>
+);
 }
