646f1a6755531d8ae4066394a266452e29885589
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-13 16:48:02 -0400

    removed vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Analytics } from '@vercel/analytics/next'; 
 
 
 export const metadata: Metadata = {
@@ -17,7 +16,6 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
-        <Analytics />
       </body>
     </html>
   );
