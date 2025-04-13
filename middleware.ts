be98aca7252b20f41ce259ae60964c29c4fca88d
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-13 19:49:11 -0400

    updated matcher

--- middleware.ts
+++ middleware.ts
@@ -41,7 +41,13 @@ export async function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    // This matcher applies middleware to all paths except API routes, Next.js internal paths, and public assets.
-    "/((?!api/|_next/|favicon.ico).*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
-};
+}
