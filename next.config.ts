231c4a7ed0f1ef0a5ec6012245d8dc49e549bbde
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-13 12:48:37 -0400

    updated config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: 'https://handshakr.duckdns.org',
+  
+  //ignore linter during build
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  output: 'standalone', 
+  
 };
 
 export default nextConfig;
