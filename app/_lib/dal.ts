c190118885e6dfd0fd9f2138ca98c64acfc6a936
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-15 01:09:53 -0400

    updated API endpoint

--- app/_lib/dal.ts
+++ app/_lib/dal.ts
@@ -8,7 +8,7 @@ import { AxiosError } from 'axios' // Import AxiosError type
 // Get user profile data
 export async function getUserProfile() {
   try {
-    const response = await axiosServer.get(`${API.PROFILE.GET}`);
+    const response = await axiosServer.get(`${API.PROFILE}`);
     return { success: true, data: response.data };
   } catch (error: unknown) {
     if (error instanceof AxiosError) {
@@ -18,44 +18,32 @@ export async function getUserProfile() {
   }
 }
 
-// Show which handshakes have not been completed
-export async function getPendingHandshakes() {
-  try {
-    const response = await axiosServer.get(`${API.HANDSHAKE.PENDING}`);
-    return { success: true, data: response.data };
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      return { success: false, error: error.response?.data?.message || "Failed to get your pending handshakes" };
-    }
-    return { success: false, error: "An unknown error occurred" };
-  }
-}
+// // Show which handshakes have not been completed
+// export async function getPendingHandshakes() {
+//   try {
+//     const response = await axiosServer.get(`${API.HANDSHAKE.PENDING}`);
+//     return { success: true, data: response.data };
+//   } catch (error: unknown) {
+//     if (error instanceof AxiosError) {
+//       return { success: false, error: error.response?.data?.message || "Failed to get your pending handshakes" };
+//     }
+//     return { success: false, error: "An unknown error occurred" };
+//   }
+// }
 
-// Get encrypted handshake history data
-export async function getHandshakeHistory() {
-  try {
-    const response = await axiosServer.get(`${API.HANDSHAKE.HISTORY}`);
-    return { success: true, data: response.data };
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      return { success: false, error: error.response?.data?.message || "Failed to get handshakes history" };
-    }
-    return { success: false, error: "An unknown error occurred" };
-  }
-}
+// // Get encrypted handshake history data
+// export async function getHandshakeHistory() {
+//   try {
+//     const response = await axiosServer.get(`${API.HANDSHAKE.HISTORY}`);
+//     return { success: true, data: response.data };
+//   } catch (error: unknown) {
+//     if (error instanceof AxiosError) {
+//       return { success: false, error: error.response?.data?.message || "Failed to get handshakes history" };
+//     }
+//     return { success: false, error: "An unknown error occurred" };
+//   }
+// }
 
-// Connect agreerer to new handshake
-export async function connectAgreerer() {
-  try {
-    const response = await axiosServer.post(`${API.HANDSHAKE.CONNECT_AGREERER}`);
-    return { success: true, data: response.data };
-  } catch (error: unknown) {
-    if (error instanceof AxiosError) {
-      return { success: false, error: error.response?.data?.message || "Failed to connect agreerer to handshake" };
-    }
-    return { success: false, error: "An unknown error occurred" };
-  }
-}
 
 /*************** PRICE ANALYZER METHODS ****************/
 
