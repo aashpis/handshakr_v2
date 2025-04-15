99fdff442d41becc612e877974895fbd2025102e
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-15 01:02:42 -0400

    removed mock data

--- app/initiated-handshakes/page.tsx
+++ app/initiated-handshakes/page.tsx
@@ -1,16 +1,5 @@
 import HandshakeCard from "@/_ui/handshake-card"
 import PageHeader from "@/_ui/page-header"
-import LoginForm from "@/_ui/login-form";
-const handshake = {
-    id: "12345",
-    initiator: "Shankar",
-    agreerer: "Ari",
-    compensation: "$20",
-    terms: "I will sell you a used computer mouse for $20",
-    status: "pending",
-    notarized: false,
-    notary: null
-}
 
 export default function Page() {
     return (
@@ -19,7 +8,6 @@ export default function Page() {
                 title="Initiated Handshake"
                 subTitle="Handshakes you have initiated"
             />
-            <HandshakeCard {...handshake} />
         </div>
 
     );
