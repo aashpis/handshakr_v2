11f85637603c2f4cd14ef113356bcee350ef3dca
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-16 17:49:24 -0400

    added  fetch for initiated and received handshakes

--- app/_lib/dal.ts
+++ app/_lib/dal.ts
@@ -6,7 +6,7 @@ import axios  from 'axios'
 export async function fetchUserProfile() {
   const csrfToken = sessionStorage.getItem("X-XSRF-TOKEN");
 
-  // Check if we have a CSRF token (basic check if user is authenticated)
+  // Basic auth check CSRF token 
   if (!csrfToken) {
     return { 
       success: false, 
@@ -15,7 +15,7 @@ export async function fetchUserProfile() {
   }
 
   try {
-    const response = await fetch(`${API.BASE}${API.PROFILE}`, {
+    const response = await fetch(`${API.BASE}/${API.PROFILE}`, {
       method: "GET",
       credentials: "include",
       headers: {
@@ -64,7 +64,7 @@ export async function getUserProfileAxiosRequest() {
   }
 
   try {
-    const response = await axios.get(`${API.BASE}${API.PROFILE}`, {
+    const response = await axios.get(`${API.BASE}/${API.PROFILE}`, {
       withCredentials: true,
       headers: {
         "Content-Type": "application/json",
@@ -95,6 +95,110 @@ export async function getUserProfileAxiosRequest() {
     };
   }
 }
+// Get User Initiated Handshakes 
+export async function fetchInitiatedHandshakes(username : string) {
+  const csrfToken = sessionStorage.getItem("X-XSRF-TOKEN");
+
+  // Check if we have a CSRF token (basic check if user is authenticated)
+  if (!csrfToken) {
+    return { 
+      success: false, 
+      error: "Not authenticated" 
+    };
+  }
+
+  try {
+    const response = await fetch(`${API.BASE}/${API.GET_MY_INITIATED_HS}/${username}`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": csrfToken,
+      }
+    });
+
+    if (response.status === 401 || response.status === 403) {
+      return { 
+        success: false, 
+        error: "Authentication Error" 
+      };
+    }
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      return { 
+        success: false, 
+        error: errorData.message || "Failed to get my initiated handshakes" 
+      };
+    }
+
+    const data = await response.json();
+    return { success: true, data };
+  } catch (error) {
+    console.error("fetchInitiatedHandshakes() error:", error);
+    return { 
+      success: false, 
+      error: error instanceof Error ? error.message : "An unknown error occurred" 
+    };
+  }
+}
+
+
+// Get User Received Handshakes 
+export async function fetchReceivedHandshakes(username : string) {
+  const csrfToken = sessionStorage.getItem("X-XSRF-TOKEN");
+
+  // Check if we have a CSRF token (basic check if user is authenticated)
+  if (!csrfToken) {
+    return { 
+      success: false, 
+      error: "Not authenticated" 
+    };
+  }
+
+  try {
+    const response = await fetch(`${API.BASE}/${API.GET_MY_RECEIVED_HS}/${username}`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": csrfToken,
+      }
+    });
+
+    if (response.status === 401 || response.status === 403) {
+      return { 
+        success: false, 
+        error: "Authentication Error" 
+      };
+    }
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      return { 
+        success: false, 
+        error: errorData.message || "Failed to get my initiated handshakes" 
+      };
+    }
+
+    const data = await response.json();
+    return { success: true, data };
+  } catch (error) {
+    console.error("fetchInitiatedHandshakes() error:", error);
+    return { 
+      success: false, 
+      error: error instanceof Error ? error.message : "An unknown error occurred" 
+    };
+  }
+}
+
+
+//axios version
+
+
+
+
+
 // // Show which handshakes have not been completed
 // export async function getPendingHandshakes() {
 //   try {
@@ -120,6 +224,10 @@ export async function getUserProfileAxiosRequest() {
 //     return { success: false, error: "An unknown error occurred" };
 //   }
 // }
+
+
+
+
 
 
 /*************** PRICE ANALYZER METHODS ****************/
