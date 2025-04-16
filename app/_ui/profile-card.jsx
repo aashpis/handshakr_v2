02a8f30d6f4eefa564446895cf102fd9da27eab9
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-16 00:14:13 -0400

    styling

--- app/_ui/profile-card.jsx
+++ app/_ui/profile-card.jsx
@@ -2,8 +2,9 @@ export default function ProfileCard({ username, userId, email }) {
    return (
      <div className="bg-white p-6 text-sm rounded-lg shadow-md">
        <div>
-         <h1 className="text-primary text-xl font-bold">Hello {username}</h1>
-         <p className="mt-2">ID#: <span className="text-primary font-bold">{userId}</span></p>
+         <h1 className="text-primary text-xl font-bold">Hello, {username}</h1>
+         <p className="text-neutral-dark italics">Welcome to Handshakr!</p>
+         <p className="mt-4">ID#: <span className="text-primary font-bold">{userId}</span></p>
          <p className="mt-2">Email: <span className="text-primary font-bold">{email}</span></p>
        </div>
      </div>
