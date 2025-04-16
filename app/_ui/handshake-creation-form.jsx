da34d97843d34788b9ccff1f14980b37387725c6
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-16 16:30:43 -0400

    error message display

--- app/_ui/handshake-creation-form.jsx
+++ app/_ui/handshake-creation-form.jsx
@@ -26,7 +26,7 @@ export default function HandshakeCreationForm() {
             className="w-full px-4 py-2 border border-neutral-light rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-light focus:border-transparent"
           />
         </div>
-        {state?.errors?.username && (
+        {state?.errors?.handshakeName && (
           <p className="text-warning italic mt-1">{state.errors.handshakeName}</p>
         )}
         <div className="mb-4">
@@ -42,22 +42,11 @@ export default function HandshakeCreationForm() {
             className="w-full px-4 py-2 border border-neutral-light rounded-lg focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
           />
         </div>
-        {state?.errors?.username && (
-          <p className="text-warning italic mt-1">{state.errors.receiverUsername}</p>
+        {state?.errors?.receiverUsername && (
+          <p className="text-warning italic mt-1">
+            {state.errors.receiverUsername}
+          </p>
         )}
-
-        {/* <div className="mb-4">
-          <label className="block text-sm font-medium  mb-2" htmlFor="compensation">
-            Compensation
-          </label>
-          <textarea
-            id="compensation"
-            name="compensation"
-            placeholder="Enter details..."
-            required
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
-          />
-        </div> */}
 
         <div className="mb-6">
           <label 
@@ -72,7 +61,7 @@ export default function HandshakeCreationForm() {
             className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary focus:border-transparent"
           />
         </div>
-        {state?.errors?.username && (
+        {state?.errors?.encryptedDetails && (
           <p className="text-warning italic mt-1">{state.errors.encryptedDetails}</p>
         )}
 
@@ -84,7 +73,11 @@ export default function HandshakeCreationForm() {
           {pending ? 'Creating Handshake...' : 'Create Handshake'}
         </button>
 
-        {state?.error && <p className="text-warning text-center font-semibold my-3">{state.error}</p>}
+        {state?.error && 
+        (<p className="text-warning text-center font-semibold my-3">
+          Handshake Creation Failed
+          </p>)
+        }
       </form>
     </div>
   )
