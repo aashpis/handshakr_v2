519eda9848f97366bfb0d45ea1b84d77ad2bdfab
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-15 23:21:32 -0400

    removed unused field

--- app/_ui/handshake-creation-form.jsx
+++ app/_ui/handshake-creation-form.jsx
@@ -13,19 +13,6 @@ export default function HandshakeCreationForm() {
       >
         <h2 className="text-2xl font-semibold text-center mb-6">Create a Handshake</h2>
 
-        {/* <div className="mb-4">
-          <label className="block text-sm font-medium  mb-2" htmlFor="initiatorUsername">
-            Initiator Username
-          </label>
-          <input
-            id="initiatorUsername"
-            name="initiatorUsername"
-            placeholder="Username"
-            required
-            className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
-          />
-        </div> */}
-
         <div className="mb-4">
           <label
             className="block text-sm font-medium  mb-2"
