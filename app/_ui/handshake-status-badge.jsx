69de68810956cdee4461fa0094ca77ae4841f3c2
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-17 02:19:44 -0400

    more status handling

--- app/_ui/handshake-status-badge.jsx
+++ app/_ui/handshake-status-badge.jsx
@@ -1,27 +1,32 @@
 export default function HandshakeStatusBadge({ status }) {
-    const sanitiizedStatus = status.toLowerCase(); // Normalize input
-   
-    const STATUS_BADGE = {
-      completed: (
-        <span className="bg-hs-completed text-xs text-white text-center py-1 px-3 rounded-full uppercase">
-          Completed
-        </span>
-      ),
-      failed: (
-        <span className="bg-hs-failed text-xs text-white text-center py-1 px-3 rounded-full uppercase">
-          Failed
-        </span>
-      ),
-      pending: (
-        <span className="bg-hs-pending text-xs text-white text-center py-1 px-3 rounded-full uppercase">
-          Pending
-        </span>
-      ),
-    }[sanitiizedStatus] ?? (
-      <span className="bg-neutral text-xs text-white text-center py-1 px-3 rounded-full uppercase">
-        Status Unknown
+  const sanitizedStatus = (status || "").toLowerCase(); // Normalize with fallback
+
+  const STATUS_BADGE = {
+    completed: (
+      <span className="bg-hs-completed text-xs text-white text-center py-1 px-3 rounded-full uppercase">
+        Completed
       </span>
-    );
-   
-    return <div>{STATUS_BADGE}</div>;
+    ),
+    accepted: (
+      <span className="bg-primary text-xs text-white text-center py-1 px-3 rounded-full uppercase">
+        Completed
+      </span>
+    ),
+    failed: (
+      <span className="bg-hs-failed text-xs text-white text-center py-1 px-3 rounded-full uppercase">
+        Failed
+      </span>
+    ),
+    pending: (
+      <span className="bg-hs-pending text-xs text-white text-center py-1 px-3 rounded-full uppercase">
+        Pending
+      </span>
+    ),
+  }[sanitizedStatus] ?? (
+    <span className="bg-neutral text-xs text-white text-center py-1 px-3 rounded-full uppercase">
+      {sanitizedStatus || "Unknown"}
+    </span>
+  );
+
+  return <div>{STATUS_BADGE}</div>;
 }
