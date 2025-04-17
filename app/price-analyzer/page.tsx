7236aaf07e00355c669771871eaddc913d81cf75
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-17 04:15:07 -0400

    styling

--- app/price-analyzer/page.tsx
+++ app/price-analyzer/page.tsx
@@ -68,7 +68,7 @@ export default function Page() {
 
   return (
     <div className="min-h-screen flex flex-col flex-start items-center gap-4 px-4">
-      <PageHeader title = "Price Analyzer" subTitle=""/>
+      <PageHeader title = "Price Analyzer" subTitle="Get statistics about the price of an item"/>
       <PriceInputForm onSubmit={handleSubmit} loading={loading} />
 
       {error && (
