ff8f3666ca2f10e8c98c256166a6c33c981f1876
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-16 22:58:03 -0400

    updated API with scraper endpts

--- app/_lib/definitions.tsx
+++ app/_lib/definitions.tsx
@@ -10,15 +10,15 @@ import { z } from 'zod'
 export const API = {
   BASE: 'https://handshakr.duckdns.org/api',
   REGISTER: 'auth/register',
-  LOGIN: 'auth/login', 
+  LOGIN: 'auth/login',
   LOGOUT: 'auth/logout',
   PROFILE: 'users/me',
-  CREATE_HANDSHAKE:"users/create-handshake",
+  CREATE_HANDSHAKE: "users/create-handshake",
   GET_MY_INITIATED_HS: "handshake/get-handshakes-by-initiator",
   GET_MY_RECEIVED_HS: "handshake/get-handshakes-by-acceptor",
   GET_PRICE_STATS: 'scraper/get-price-stats',
-  ITEM_PRICE_HISTOGRAM: 'scraper/graph-item-price-histogram',
-  WEEKLY_MEDIAN_PRICE: 'graph-item-weekly-median-price',
+  GET_PRICE_HISTOGRAM: 'scraper/graph-item-price-histogram',
+  GET_WEEKLY_MEDIAN_PRICE: 'graph-item-weekly-median-price',
 }
 
 /**********  SCHEMA DEFINTIONS  *******************
@@ -29,16 +29,16 @@ export const API = {
 // validate user registration-form input 
 // returns error messages to be displayed in form
 export const UserRegisterFormSchema = z.object({
-  
+
   username: z
     .string()
-    .regex(/^\S*$/, { message: 'Username cannot contain whitespace' }) 
+    .regex(/^\S*$/, { message: 'Username cannot contain whitespace' })
     .min(5, { message: 'Name must be at least 5 characters long.' })
-    .max(30, )
+    .max(30,)
     .trim(),
-  
-    email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
-  
+
+  email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
+
   password: z
     .string()
     .min(8, { message: 'Be at least 8 characters long' })
@@ -49,17 +49,17 @@ export const UserRegisterFormSchema = z.object({
     })
     .trim(),
 
-    confirmPassword: z.string().trim()
+  confirmPassword: z.string().trim()
 
-  })
+})
   //check if passwords match
   .refine(
     (data) => data.password === data.confirmPassword,
     {
       message: "The passwords did not match.",
       path: ["confirmPassword"],
-  }
-);
+    }
+  );
 
 // validate login-form input
 // makes sure values are strings and not null
@@ -68,10 +68,10 @@ export const LoginFormSchema = z.object({
   username: z
     .string()
     .trim()
-    .min(1, { message: "Username is required" }), 
+    .min(1, { message: "Username is required" }),
   password: z
     .string()
-    .min(1, { message: "Password is required" }) 
+    .min(1, { message: "Password is required" })
 });
 
 // validate handshake-creation-form input
@@ -86,7 +86,7 @@ export const HandshakeFormSchema = z.object({
     .trim(),
   encryptedDetails: z
     .string()
-    .min(10, {message: "Make sure handshake has enough details"})
+    .min(10, { message: "Make sure handshake has enough details" })
     .trim()
 })
 
@@ -98,36 +98,36 @@ export const HandshakeFormSchema = z.object({
 // if no errors messages, it is set as undefined 
 export type UserAuthFormState =
   | {
-      errors?: {
-        name?: string[]
-        email?: string[]
-        password?: string[]
-      }
-      message?: string
+    errors?: {
+      name?: string[]
+      email?: string[]
+      password?: string[]
     }
+    message?: string
+  }
   | undefined
 
 
-  export type HandshakeFormState =
+export type HandshakeFormState =
   | {
-      errors?: {
-        handshakeName?: string[];
-        encryptedDetails?: string[];
-        receiverUsername?: string[];
-      };
-      message?: string;
-    }
+    errors?: {
+      handshakeName?: string[];
+      encryptedDetails?: string[];
+      receiverUsername?: string[];
+    };
+    message?: string;
+  }
   | undefined;
 
 
-  
+
 export type UserData =
-| {
-  id: string;
-  username: string;
-  email: string;
-} 
-| undefined;
+  | {
+    id: string;
+    username: string;
+    email: string;
+  }
+  | undefined;
 
 
 export type Handshake = {
@@ -140,3 +140,11 @@ export type Handshake = {
   acceptorUsername: string;
 }
 
+
+export type PriceStats = {
+  itemName: string,
+  max: string,
+  mean: string,
+  median: string,
+  min: string,
+}
