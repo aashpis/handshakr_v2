07d1fa628927ab4e851744f9ea805683416d8068
aashpis <37486859+aashpis@users.noreply.github.com>
2025-04-16 20:34:39 -0400

    responsive logo for mobile

--- app/_ui/sidenav.jsx
+++ app/_ui/sidenav.jsx
@@ -6,7 +6,7 @@ import NavLinks from './nav-links';
 import LogoutButton from './logout-button';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
-import { Menu, X } from 'lucide-react'; 
+import { Menu, X } from 'lucide-react';
 
 export default function SideNav() {
   const router = useRouter();
@@ -21,8 +21,17 @@ export default function SideNav() {
             src="/hs_icon.png"
             width={1250}
             height={625}
-            alt="handshake"
-            className="object-scale-down h-10 md:h-20"
+            alt="handshake desktop logo"
+            className="hidden md:block object-scale-down h-10 md:h-20"
+          />
+
+          {/* Mobile / Top nav mode (below md) */}
+          <Image
+            src="/handshakr-banner.png"
+            width={1600}
+            height={512}
+            alt="handshake mobile logo"
+            className="block md:hidden object-scale-down h-10"
           />
         </Link>
 
@@ -37,16 +46,15 @@ export default function SideNav() {
 
       {/* Navigation links (collapsible on mobile) */}
       <div
-        className={`flex-col space-y-2 px-4 md:flex md:flex-grow md:space-y-2 md:px-0 ${
-          menuOpen ? 'flex' : 'hidden'
-        }`}
-      > 
-      
+        className={`flex-col space-y-2 px-4 md:flex md:flex-grow md:space-y-2 md:px-0 ${menuOpen ? 'flex' : 'hidden'
+          }`}
+      >
+
         {/* passing method to close Hamburger menu on click */}
-        <NavLinks clickHandler={() => setMenuOpen(false)}  />
+        <NavLinks clickHandler={() => setMenuOpen(false)} />
         <div className="hidden h-auto w-full grow md:block"></div>
 
-        <LogoutButton/>
+        <LogoutButton />
       </div>
     </div>
   );
